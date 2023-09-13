6cbe852a035774ad902c9bfec455e32f32928f49
Ecaterina Ilincu <ilincue@gmail.com>
2023-09-13 18:00:22 +0300

    Added variables and commments task

---
+++ src/main/java/md/tekwill/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,37 @@
+package md.tekwill.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+        // Declare primitive data type variables used house as an examples
+
+        double housePrice;
+        byte numBathrooms;
+        byte numBedrooms;
+        char houseColor; // color code
+        int yearBuilt;
+        int zipCode;
+        short numFloors;
+        short numWindows;
+        long mortgageAmount;
+        float squareFootage;
+        boolean isForSale;
+        boolean isInChisinau;
+
+        //Initialization of the above variables
+
+        housePrice = 1000000d;
+        numFloors = 4;
+        numBedrooms = 3;
+        numBathrooms = 3;
+        houseColor = 'A';
+        yearBuilt = 2010;
+        zipCode = 4000;
+        numWindows = 10;
+        mortgageAmount = 70000;
+        squareFootage = 600.6f;
+        isForSale = true;
+        isInChisinau = false;
+
+
+    }
+}
